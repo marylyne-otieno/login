a10d61f68e91e6d3ecb981e98f25db83d47e0abe
marylyne <otienomarylyne76@gmail.com>
2025-06-22 00:22:18 +0300

    Configure Axios with baseURL and credentials for JWT auth

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,5 +1,4 @@
 
-// api.ts
 import axios from 'axios';
 
 const API = axios.create({
@@ -7,6 +6,7 @@ const API = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, 
 });
 
 export default API;
